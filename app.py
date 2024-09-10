c48fa9954e5aeb7056eb7f789505f267deada5bc
christiansanchez-SE <christiansanchez714@gmail.com>
2024-09-10 13:42:18 -0400

    added functon to add movies

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import random
 
 app = Flask(__name__)
 
-movies = ['Shawn of the Dead', 'Treasure Planet', 'Zombieland', 'Interstellar', 'Monsters Inc.', 'Shrek', 'Nacho Libre']
+# List of movies
+movies = ['Shawn of the Dead', 'Treasure Planet', 'Zombieland', 'Interstellar', 'Lord of the Rings', 'Star Wars', 'Scott Pilgrim vs. the World']
 
+# Route to serve the main page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', movies=movies)
 
+# Route to get a random movie
 @app.route('/random-movie')
 def pick_random_movie():
-    return random.choice(movies)
+    movie = random.choice(movies)
+    return jsonify(movie=movie)
+
+# Route to add a new movie
+@app.route('/add-movie', methods=['POST'])
+def add_movie():
+    new_movie = request.form['movie']
+    if new_movie and new_movie not in movies:
+        movies.append(new_movie)
+        return jsonify(success=True, movie=new_movie)
+    return jsonify(success=False, message="Movie is already in the list or invalid input.")
 
 if __name__ == "__main__":
     app.run(debug=True)
